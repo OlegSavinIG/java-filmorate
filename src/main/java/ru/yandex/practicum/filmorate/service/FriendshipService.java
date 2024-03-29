b50d8c60e77019f280249f389f7bd9d6f1b90080
Oleg <134596179+OlegSavinIG@users.noreply.github.com>
2024-03-30 02:40:40 +0300

    friendshipsservice

--- src/main/java/ru/yandex/practicum/filmorate/service/FriendshipService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FriendshipService.java
@@ -1,13 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.FriendshipsStorage;
-import ru.yandex.practicum.filmorate.exception.NotExistException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.List;
 import java.util.Set;
 
 @Service
