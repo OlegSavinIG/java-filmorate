f384a732f2951dc967e38e58ee6e0c476430f0de
Oleg <134596179+OlegSavinIG@users.noreply.github.com>
2024-03-29 21:32:18 +0300

    model changes

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Genre {
     private int id;
--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode
 @NoArgsConstructor
 public class MpaRating {
     private int id;
